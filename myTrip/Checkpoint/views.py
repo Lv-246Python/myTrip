f8e203c734999679415734260ce5e05b522b60d1
KarnaushenkoVO <KarnaushenkoVO@gmail.com>
2017-07-07 14:25:47 +0300

    model corrected to PEP8

--- myTrip/Checkpoint/views.py
+++ myTrip/Checkpoint/views.py
@@ -1,31 +1,31 @@
-from django.shortcuts import render
+""" This checkpoint module generates view for CRUD requests"""
+
+import json
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
 from .models import  Checkpoint
-import json
+
 
 class CheckpointView(View):
     """ Checkpoint view handle GET, POST, PUT, DELETE requests """
 
-    def get(self, request, checkpoint_id = None):
-        if not checkpoint_id:
-            checkpoint_objects = Checkpoint.get_all()
-            result = [check.to_dict() for check in checkpoint_objects]
-            return JsonResponse(result, safe=False, status=200)
-
+    def get(self, request, checkpoint_id):
+        """ Handles get request"""
         checkpoint_object = Checkpoint.get_by_id(checkpoint_id)
-        if checkpoint_object:
-            result = checkpoint_object.to_dict()
-            return JsonResponse(result, safe=False, status=200)
-        return HttpResponse(status=404)
+        if not checkpoint_object:
+            return HttpResponse(status=404)
+        result = checkpoint_object.to_dict()
+        return JsonResponse(result, status=200)
 
-    def post(self,request):
+    def post(self, request):
+        """ Handles post request"""
         data = json.loads(request.body.decode('utf-8'))
         if not Checkpoint.post(data):
             return HttpResponse(status=400)
         return HttpResponse(status=200)
 
-    def put(self, request,checkpoint_id):
+    def put(self, request, checkpoint_id):
+        """Handles put request"""
         if not checkpoint_id:
             return HttpResponse(status=400)
         checkpoint_object = Checkpoint.get_by_id(checkpoint_id)
@@ -38,6 +38,7 @@ class CheckpointView(View):
         return HttpResponse(status=200)
 
     def delete(self, request, checkpoint_id):
+        """ Handles delete request"""
         if not checkpoint_id:
             return HttpResponse(status=400)
         checkpoint_object = Checkpoint.get_by_id(checkpoint_id)
