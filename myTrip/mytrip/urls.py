79743aedf05b17b07a5eac080c18c4c6ee418735
PanLeon <leonpan77@gmail.com>
2017-07-07 21:15:20 +0300

    rebase dev'

--- myTrip/mytrip/urls.py
+++ myTrip/mytrip/urls.py
@@ -13,16 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-<<<<<<< e7864a61d4a9eb231462cbbc89928f3e141e85c3
 
 from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^api/v1/comment/', include('comment.urls')),
-=======
-from django.conf.urls import url,include
-
-urlpatterns = [
->>>>>>> edit urls
     url(r'^trip/', include('create_trip.urls')),
 ]
