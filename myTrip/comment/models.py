bb6cb4b155372099cef9801755404daf53b2cbe3
yuriiZherebukh <yura7887@gmail.com>
2017-07-07 10:46:07 +0300

    Corrected related to code style

--- myTrip/comment/models.py
+++ myTrip/comment/models.py
@@ -1,54 +1,52 @@
+"""This module contains comment model class and basic functions."""
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 
 class Comment(models.Model):
+    """
+     Comment
+     :argument id: int - auto generated primary key
+     :argument message: str - comment message
+     :argument user: int - ToDo foreign key to User model
+    ."""
     message = models.TextField()
-    user_id = models.IntegerField()
-    photo_id = models.IntegerField()
-    checkpoint_id = models.IntegerField()
-    trip_id = models.IntegerField()
+    user = models.IntegerField()
 
     @staticmethod
-    def get_by_id(id):  # method to get one element by his id, uses to views, returns None when exception works
+    def get_by_id(comment_id):
+        """
+        ToDo method to get one element by his id, uses to views,
+        returns None when exception works.
+        """
         try:
-            return Comment.objects.get(id=id)
-        except Exception as error:
+            return Comment.objects.get(id=comment_id)
+        except ObjectDoesNotExist:
             return None
 
-    @staticmethod
-    def get_all():  # method to get all objects by its id's uses for views
-        return Comment.objects.all()
-
-    def to_dict(self, request=None):  # method rebuilds queryset ot object dictionary for our views
+    def to_dict(self):
+        """
+        ToDO method rebuilds queryset ot object dictionary for our views.
+        """
         return {
             'id': self.id,
-            'message': self.message,  # returns object, need to return his name
-            'user_id': self.user_id,
-            'photo_id': self.photo_id,
-            'checkpoint_id': self.checkpoint_id,
-            'trip_id': self.trip_id
+            'message': self.message,
+            'user_id': self.user,
         }
 
-    def create(self, message, user_id, photo_id, checkpoint_id, trip_id):
+    def create(self, message, user):
+        """ToDo.
+        """
         self.message = message
-        self.user_id = user_id
-        self.photo_id = photo_id
-        self.checkpoint_id = checkpoint_id
-        self.trip_id = trip_id
+        self.user = user
         self.save()
 
-    def update(self, message, user_id, photo_id, checkpoint_id, trip_id):
+    def update(self, message):
+        """ToDo"""
         if message:
             self.message = message
-        if user_id:
-            self.user_id = user_id
-        if photo_id:
-            self.photo_id = photo_id
-        if checkpoint_id:
-            self.checkpoint_id = checkpoint_id
-        if trip_id:
-            self.trip_id = trip_id
         self.save()
 
-    def __str__(self):
-        return "{} {} {} {} {}".format(self.message, self.user_id, self.photo_id, self.checkpoint_id, self.trip_id)
+    def __repr__(self):
+        return "{} {} {}".format(self.id, self.message, self.user)
