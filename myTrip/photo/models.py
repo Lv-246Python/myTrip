d9dbc95fd7d42608ee68f24292b81a7fcb3332ae
Ashiato <regens4@gmail.com>
2017-07-08 19:30:57 +0300

    pylint 9.33/10

--- myTrip/photo/models.py
+++ myTrip/photo/models.py
@@ -1,34 +1,40 @@
+"""Module contain photo model class and methods."""
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
+
 class Photo(models.Model):
+    """
+    src = photo source link,
+    user_id = id user who post.
+    """
     src = models.CharField(max_length=200)
     user_id = models.IntegerField()
 
     @staticmethod
     def get_by_id(photo_id):
+        """Method return one element by id or None if exception."""
         try:
             return Photo.objects.get(id=photo_id)
-        except Exception:
+        except ObjectDoesNotExist:
             return None
 
     def create(self, src, user_id):
+        """Creating photo model."""
         self.src = src
         self.user_id = user_id
         self.save()
-
 
     def update(self, src, user_id):
+        """Updating photo model."""
         self.src = src
         self.user_id = user_id
         self.save()
 
-
     def to_dict(self):
+        """Convert queryset to dictionary."""
         return {
             "id": self.id,
             "src": self.src,
             "user_id": self.user_id,
         }
-
-    def __str__(self):
-        return "{} {} ".format(self.src, self.user_id)
--- myTrip/photo/views.py
+++ myTrip/photo/views.py
@@ -1,12 +1,14 @@
+"""Module generate view for photo requests."""
 import json
 from django.views.generic.base import View
-from .models import Photo
 from django.http import JsonResponse, HttpResponse
-
+from .models import Photo
 
 class PhotoView(View):
+    """Class that handle HTTP requests."""
 
     def get(self, request, photo_id):
+        """GET request handler."""
         photo = Photo.get_by_id(photo_id)
         if not photo:
             return HttpResponse(status=404)
@@ -14,12 +16,14 @@ class PhotoView(View):
         return JsonResponse(photo, status=200, safe=False)
 
     def post(self, request):
+        """POST request hangler."""
         post_data = json.loads(request.body.decode('utf-8'))
         photo = Photo()
         photo.create(**post_data)
         return HttpResponse(status=200)
 
     def put(self, request, photo_id):
+        """PUT request hangler."""
         photo = Photo.get_by_id(photo_id)
         if not photo:
             return HttpResponse(status=404)
@@ -28,7 +32,7 @@ class PhotoView(View):
         return HttpResponse(status=200)
 
     def delete(self, request, photo_id):
+        """DELETE request handler."""
         photo = Photo.get_by_id(photo_id)
         photo.delete()
         return HttpResponse(status=200)
-
