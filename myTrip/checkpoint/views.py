a7d38b2003182a3dce0207e8f691d99c84d5380e
KarnaushenkoVO <KarnaushenkoVO@gmail.com>
2017-07-10 21:43:22 +0300

    Model's methods have been renamed

--- myTrip/checkpoint/views.py
+++ myTrip/checkpoint/views.py
@@ -10,7 +10,11 @@ class CheckpointView(View):
     """ checkpoint view handle GET, POST, PUT, DELETE requests """
 
     def get(self, request, checkpoint_id):
-        """ Handles get request"""
+        """
+        Handles get request
+        Return json and status 200 with new object if operation was successful
+        Return status 404 if checkpoint with such checkpoint_id wasn't found
+        """
         checkpoint_object = Checkpoint.get_by_id(checkpoint_id)
         if not checkpoint_object:
             return HttpResponse(status=404)
@@ -18,32 +22,48 @@ class CheckpointView(View):
         return JsonResponse(result, status=200)
 
     def post(self, request):
-        """ Handles post request"""
+        """
+         Handles post request
+         Create new object and returns status  200 if all was successful
+         Returns status 409 if creation doesn't occur
+         """
         data = json.loads(request.body.decode('utf-8'))
-        if not Checkpoint.post(data):
-            return HttpResponse(status=400)
+        if not Checkpoint.create(data):
+            return HttpResponse(status=409)
         return HttpResponse(status=200)
 
     def put(self, request, checkpoint_id):
-        """Handles put request"""
+        """
+        Handles put request
+        Return status 200 if checkpoint has been successful updated
+        Return status 400 if checkpoint_id wasn't passed
+        Return status 404 if checkpoint with such checkpoint_id wasn't found
+        Return status 200 if checkpoint wasn't updated
+        """
         if not checkpoint_id:
             return HttpResponse(status=400)
         checkpoint_object = Checkpoint.get_by_id(checkpoint_id)
         if not checkpoint_object:
-            return HttpResponse(status=400)
+            return HttpResponse(status=404)
         data = json.loads(request.body.decode('utf-8'))
-        result = checkpoint_object.put(data)
+        result = checkpoint_object.update(data)
         if not result:
-            return HttpResponse(status=400)
+            return HttpResponse(status=204)
         return HttpResponse(status=200)
 
     def delete(self, request, checkpoint_id):
-        """ Handles delete request"""
+        """
+        Handles delete request
+        Returns 200 if checkpoint has been deleted
+        Returns 400 if checkpoint id hasn't been passed
+        Returns 404 if checkpoint with such checkpoint_id wasn't found
+        Returns 500 if checkpoint wasn't deleted for some reasons
+        """
         if not checkpoint_id:
             return HttpResponse(status=400)
         checkpoint_object = Checkpoint.get_by_id(checkpoint_id)
         if not checkpoint_object:
-            return HttpResponse(status=400)
+            return HttpResponse(status=404)
         result = checkpoint_object.delete_checkpoint()
         if not result:
             return HttpResponse(status=500)
