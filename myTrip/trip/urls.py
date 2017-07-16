373ce764c26ae71e22eb1a87dd05331cd0d2c02e
PanLeon <leonpan77@gmail.com>
2017-07-16 15:10:56 +0300

    convert tabs to spaces

--- myTrip/trip/urls.py
+++ myTrip/trip/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.TripView.as_view()),
+    url(r'^$', views.TripView.as_view()),
     url(r'^(?P<trip_id>\d+)/$', views.TripView.as_view()),
 ]
