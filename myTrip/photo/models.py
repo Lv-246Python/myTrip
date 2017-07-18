ca05a2307fe53ac377ca1c2042390eaf34d8f158
Ashiato <regens4@gmail.com>
2017-07-18 21:14:49 +0300

    fixed methods, added new lines after imports

--- myTrip/photo/models.py
+++ myTrip/photo/models.py
@@ -2,7 +2,9 @@
 
 from datetime import datetime
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+
 from checkpoint.models import Checkpoint
 from registration.models import CustomUser
 from trip.models import Trip
@@ -13,9 +15,9 @@ class Photo(models.Model):
     Photo
     :argument id: int - auto generated primary key
     :argument src: url - photo source link
-    :argument user: int - foreign key to User model id
-    :argument trip_id: int - foreign key to Trip model id
-    :argument checkpoint_id: int - foreign to Checkpoint model id
+    :argument user: - foreign key to User model
+    :argument trip: - foreign key to Trip model
+    :argument checkpoint: - foreign to Checkpoint model
     :argument description: str - description to photo
     :argument created_at: date - time when created
     :argument updated_at: date - time when updated.
@@ -40,36 +42,11 @@ class Photo(models.Model):
         """
         try:
             return Photo.objects.get(id=photo_id)
-        except Photo.DoesNotExist:
+        except ObjectDoesNotExist:
             return None
 
     @staticmethod
-    def get_by_trip_id(trip_id):
-        """
-        Get photo with given trip id
-        Args:
-            trip_id (int): trip id.
-        Returns:
-            QuerySet<Photos>: QuerySet of Photos.
-        """
-        photos = Photo.objects.filter(trip_id=trip_id)
-        return photos
-
-    @staticmethod
-    def get_by_photo_and_user_id(photo_id, user_id):
-        """
-        Get photo with given checkpoint id
-        Args:
-            photo_id (int): photo id
-            user_id (int): user id
-        Returns:
-            Object<Photos>: Object of Photo.
-        """
-        photo = Photo.objects.get(id=photo_id, user_id=user_id)
-        return photo
-
-    @staticmethod
-    def get_by_trip_id_and_checkpoint_id(trip_id, checkpoint_id):
+    def filter(trip_id, checkpoint_id):
         """
         Get photo with given trip and checkpoint id
         Args:
@@ -78,27 +55,26 @@ class Photo(models.Model):
         Returns:
             QuerySet<Photos>: QuerySet of Photos.
         """
-        photos = Photo.objects.filter(trip_id=trip_id, checkpoint_id=checkpoint_id)
-        return photos
-
+        try:
+            return Photo.objects.filter(trip_id=trip_id, checkpoint_id=checkpoint_id)
+        except ObjectDoesNotExist:
+            return None
 
     @staticmethod
-    def create(src, user_id, trip_id=None, checkpoint_id=None, description=None):
+    def create(src, user, description, trip=None, checkpoint=None):
         """ Creating photo model, and returns created object"""
         photo = Photo()
         photo.src = src
-        if trip_id:
-            photo.trip_id = Trip.get_by_id(trip_id).id
-        if checkpoint_id:
-            photo.checkpoint_id = Checkpoint.get_by_id(checkpoint_id).id
-        photo.user = CustomUser.get_by_id(user_id)
+        photo.trip = trip
+        photo.checkpoint = checkpoint
+        photo.user = user
         photo.description = description
         photo.save()
         return photo
 
-    def update(self, user_id, description=None):
+    def update(self, description):
         """Updating photo description."""
-        if description and user_id:
+        if description:
             self.description = description
             self.updated_at = datetime.now()
         self.save()
@@ -115,15 +91,15 @@ class Photo(models.Model):
                     'checkpoit_id': checkpoint id,
                     'description': description text,
                     'created': time when created,
-                    'last updated': time when las updated
+                    'last updated': time when last updated
                 }
         """
         return {
             "id": self.id,
             "src": self.src,
             "user": self.user.id,
-            "trip_id": self.trip_id,
-            "checkpoint_id": self.checkpoint_id,
+            "trip_id": self.trip.id if self.trip else None,
+            "checkpoint_id": self.checkpoint.id if self.checkpoint else None,
             "description": self.description,
             "created": self.created_at,
             "last updated": self.updated_at
--- myTrip/photo/views.py
+++ myTrip/photo/views.py
@@ -4,37 +4,47 @@ import json
 
 from django.views.generic.base import View
 from django.http import JsonResponse, HttpResponse
-from .models import Photo
+
+from checkpoint.models import Checkpoint
 from registration.models import CustomUser
+from trip.models import Trip
+from .models import Photo
+from datetime import datetime
 
 
 class PhotoView(View):
     """Class that handle HTTP requests."""
 
-    def get(self, request, trip_id=None, checkpoint_id=None):
+    def get(self, request, trip_id=None, checkpoint_id=None, photo_id=None):
         """GET request handler. Can return photo/photos ordered by given args."""
-        if trip_id and checkpoint_id:
-            photos = Photo.get_by_trip_id_and_checkpoint_id(trip_id, checkpoint_id)
-        photos = Photo.get_by_trip_id(trip_id)
-        data = [photo.to_dict() for photo in photos]
+        if not photo_id:
+            photos = Photo.filter(trip_id, checkpoint_id)
+            if not photos:
+                return HttpResponse(status=404)
+            data = [photo.to_dict() for photo in photos]
+            return JsonResponse(data, status=200, safe=False)
+        photo = Photo.get_by_id(photo_id)
+        print(datetime.now())
+        if not photo:
+            return HttpResponse(status=404)
+        data = photo.to_dict()
         return JsonResponse(data, status=200, safe=False)
-
 
     def post(self, request, trip_id=None, checkpoint_id=None):
         """POST request hangler.Creating a new photo object and return status 201("created")"""
         post_data = json.loads(request.body.decode('utf-8'))
-        if trip_id and checkpoint_id:
-            photo = Photo.create(trip_id=trip_id, checkpoint_id=checkpoint_id, **post_data)
-            data = photo.to_dict()
-            return JsonResponse(data, status=201)
-        photo = Photo.create(trip_id=trip_id, **post_data)
+        user = CustomUser.get_by_id(post_data["user"])
+        trip = Trip.get_by_id(trip_id)
+        checkpoint = Checkpoint.get_by_id(checkpoint_id)
+        photo = Photo.create(trip=trip, checkpoint=checkpoint,
+                             user=user, src=post_data["src"], description=post_data["description"])
         data = photo.to_dict()
         return JsonResponse(data, status=201)
 
-    def put(self, request, trip_id=None, checkpoint_id=None, photo_id=None):  # pylint: disable=no-self-use
+    def put(self, request, trip_id=None, checkpoint_id=None, photo_id=None):  # pylint: disable=unused-argument,no-self-use
         """PUT request hangler. If photo object found by id, try to update photo."""
         update_data = json.loads(request.body.decode('utf-8'))
-        photo = Photo.get_by_photo_and_user_id(photo_id, update_data["user_id"])
+        photo = Photo.get_by_id(photo_id)
         if not photo:
             return HttpResponse(status=404)
         photo.update(**update_data)
@@ -43,8 +53,7 @@ class PhotoView(View):
 
     def delete(self, request, trip_id=None, checkpoint_id=None, photo_id=None):  # pylint: disable=unused-argument,no-self-use
         """DELETE request handler.If photo were found by id, try to delete photo."""
-        data = json.loads(request.body.decode('utf-8'))
-        photo = Photo.get_by_photo_and_user_id(photo_id, data["user_id"])
+        photo = Photo.get_by_id(photo_id)
         if not photo:
             return HttpResponse(status=404)
         photo.delete()
