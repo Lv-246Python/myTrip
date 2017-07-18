af369bfa647c3ecf4a96854f06462163c5403f2c
Ashiato <regens4@gmail.com>
2017-07-18 11:17:07 +0300

    fixed update_at

--- myTrip/photo/views.py
+++ myTrip/photo/views.py
@@ -1,6 +1,7 @@
 """Module generate view for photo requests."""
 
 import json
+
 from django.views.generic.base import View
 from django.http import JsonResponse, HttpResponse
 from .models import Photo
@@ -14,12 +15,10 @@ class PhotoView(View):
         """GET request handler. Can return photo/photos ordered by given args."""
         if trip_id and checkpoint_id:
             photos = Photo.get_by_trip_id_and_checkpoint_id(trip_id, checkpoint_id)
-            data = [photo.to_dict() for photo in photos]
-            return JsonResponse(data, status=200, safe=False)
-        if trip_id:
-            photos = Photo.get_by_trip_id(trip_id)
-            data = [photo.to_dict() for photo in photos]
-            return JsonResponse(data, status=200, safe=False)
+        photos = Photo.get_by_trip_id(trip_id)
+        data = [photo.to_dict() for photo in photos]
+        return JsonResponse(data, status=200, safe=False)
+
 
     def post(self, request, trip_id=None, checkpoint_id=None):
         """POST request hangler.Creating a new photo object and return status 201("created")"""
@@ -35,7 +34,7 @@ class PhotoView(View):
     def put(self, request, trip_id=None, checkpoint_id=None, photo_id=None):  # pylint: disable=no-self-use
         """PUT request hangler. If photo object found by id, try to update photo."""
         update_data = json.loads(request.body.decode('utf-8'))
-        photo = Photo.get_by_id(photo_id, update_data["user_id"])
+        photo = Photo.get_by_photo_and_user_id(photo_id, update_data["user_id"])
         if not photo:
             return HttpResponse(status=404)
         photo.update(**update_data)
@@ -45,8 +44,8 @@ class PhotoView(View):
     def delete(self, request, trip_id=None, checkpoint_id=None, photo_id=None):  # pylint: disable=unused-argument,no-self-use
         """DELETE request handler.If photo were found by id, try to delete photo."""
         data = json.loads(request.body.decode('utf-8'))
-        photo = Photo.get_by_id(photo_id, data["user_id"])
+        photo = Photo.get_by_photo_and_user_id(photo_id, data["user_id"])
         if not photo:
-            return  HttpResponse(status=404)
+            return HttpResponse(status=404)
         photo.delete()
         return HttpResponse(status=200)
--- myTrip/tests/unittests/photo/test.py
+++ myTrip/tests/unittests/photo/test.py
@@ -3,7 +3,3 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class PhotoViewTests(TestCase):
-    def test_create(self):
-        response = self.client.post(reverse('PhotoView'),
-                                    json.dumps(""))
