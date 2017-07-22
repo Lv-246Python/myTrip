ee162bfb9c57d10c099b5578740dc075035feb05
PanLeon <leonpan77@gmail.com>
2017-07-22 17:23:08 +0300

    post,put,delete only for logged users

--- myTrip/tests/unittests/trip/test.py
+++ myTrip/tests/unittests/trip/test.py
@@ -12,18 +12,17 @@ class ViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.client = Client()
         user = CustomUser.objects.create(id=10, email = 'ln@gmail.com', password = 'root')
         trip = Trip.objects.create(id=10, user = user, title = 'title', description = 'description', status = 0,
                     create_at=(2017, 7, 20, 11, 38, 34, 466455),
                     update_at=(2017, 7, 20, 11, 38, 34, 466455))
+        client = Client(user)
+        client.login(username="ln@gmail.com", password="root")
 
     def test_get_by_id_success(self):
         response = self.client.get('/api/v1/trip/10/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 6)
-        response = self.client.get('/api/v1/trip/')
-        self.assertEqual(response.status_code, 200)
 
     def test_get_by_id_error(self):
         response = self.client.get('/api/v1/trip/2/')
@@ -35,7 +34,6 @@ class ViewTest(TestCase):
 
     def test_post(self):
         response = self.client.post('/api/v1/trip/', json.dumps({
-                    "user": 1,
                     "title": 'title',
                     "description": "some text",
                     "status": 0}),
