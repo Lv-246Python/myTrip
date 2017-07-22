ed0365c83aa6689de44f5a5a82e923fd85ca30b0
tayrus <artem.chornyi.ua@gmail.com>
2017-07-22 19:03:20 +0300

    changed like app to one data base, with correct POST and Get methods, but incorrect DELETE method

--- myTrip/like/urls_photo.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import url
-from .views import LikesPhotoView
-
-urlpatterns = [url(r'^$', LikesPhotoView.as_view(), name='like_ph'),
-               url(r'^(?P<like_id>\d+)/$', LikesPhotoView.as_view(), name='like_ph')]
