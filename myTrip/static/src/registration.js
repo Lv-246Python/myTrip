6ebbe9ec6eec0df27f2db59106dc92ee20f943bd
KarnaushenkoVO <KarnaushenkoVO@gmail.com>
2017-07-26 10:00:38 +0300

    Created registration page

--- myTrip/static/src/registration.js
+++ myTrip/static/src/registration.js
@@ -1,44 +1,11 @@
 import React from "react";
 import axios from "axios"
 
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/FlatButton';
 
-class Registration_header extends React.Component {
-    render() {
-        return (
-            <div className='registration_header'>
-                    <img src="static/img/logo.png" alt="logo" /><h1>TripTracker.com</h1>
-                    <p>Get started for free</p>
-            </div>
-        );
-    }
-}
-
-class Registration_info extends React.Component {
-    render() {
-        return (
-            <div className='registration_info'>
-                <div className='info_el'>
-                    <p className='info_label'>Fun</p>
-                    <p className='info_note'>
-                        Get a ton of fun exploring the worldwhile sharing your journey with the rest of the world
-                    </p>
-                </div>
-                <div className='info_el'>
-                    <p className='info_label'>Easy</p>
-                    <p className='info_note'>
-                        Easy to use. User-friendly interface
-                    </p>
-                </div>
-                <div className='info_el'>
-                    <p className='info_label'>Connect</p>
-                    <p className='info_note'>
-                        With soical networks it's even easier with automatic checkpoint and media updates
-                    </p>
-                </div>
-            </div>
-        );
-    }
-}
 class Registration_form extends React.Component {
     constructor(props) {
         super(props);
@@ -75,30 +42,35 @@ class Registration_form extends React.Component {
     }
     render() {
         return (
-            <div className='registration_form'>
-                <h2>Registration form</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <ul className='flex-outer'>
-                        <li>
-                            <label htmlFor='email'>Email:</label>
-                            <input type="text" value={this.state.email} onChange={this.handleEmail} id='email'/>
-                        </li>
-                        <li>
-                            <label htmlFor='password'>Password:</label>
-                            <input type="password" value={this.state.password} onChange={this.handlePassword}
-                             id='password'/>
-                        </li>
-                        <li>
-                            <input type="submit" id='Button' value="Create free account" />
-                        </li>
-                    </ul>
-                </form>
-                <p>or get yourself started with social networks</p>
-                <div className='sc_buttons'>
-                    <button className='sc_button fb_button'></button>
-                    <button className='sc_button insta_button'></button>
-                    <button className='sc_button google_button'></button>
-                </div>
+            <div className='form_fields'>
+                <h1>REGISTRATION</h1>
+                <TextField
+                    hintText="Name"
+                    name="first_name"
+                />
+                <TextField
+                    hintText="Surname"
+                    name="last_name"
+                />
+                <TextField
+                    hintText="Email"
+                    name="email"
+                    type="email"
+                />
+                <TextField
+                    hintText="Password"
+                    name="password"
+                    type='password'
+                />
+                <RaisedButton label="Sign UP"
+                    primary={true}
+                    style={{
+                        margin:"5%"
+                    }}
+                    labelStyle = {{
+                        fontSize:"1.2em"
+                    }}
+                 />
             </div>
         );
     }
@@ -106,14 +78,16 @@ class Registration_form extends React.Component {
 export default class Registration extends React.Component {
     render() {
         return (
-            <div>
-                < Registration_header />
-                <div className='centerBlock'>
-                    < Registration_form history = {this.props.history} />
-                    <div className='vertical-line'></div>
-                    < Registration_info />
-                </div>
-            </div>
+            <Paper
+            style = {{
+                  margin:"5% auto",
+                  width:"40%",
+            }}
+            zDepth={2} >
+
+                  <Registration_form history = {this.props.history} />
+
+            </Paper>
         );
     }
 }
