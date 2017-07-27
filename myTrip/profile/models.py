257c701e0a9d916291464f708882e2dd655c4c25
Spycheast <spycheast@gmail.com>
2017-07-27 16:07:26 +0300

    Fixed models

--- myTrip/profile/models.py
+++ myTrip/profile/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     """
     Profile
     :argument id: int - auto generated primary key
-    :argument user: int - foreign key to User model
+    :argument user: int - one-to-one relation to User model
     :argument first_name: str - first name of the user
     :argument last_name: str - last name of the user
     :argument avatar: url - avatar source link
@@ -20,9 +20,10 @@ class Profile(models.Model):
     :argument instagram: url - instagram profile link
     :argument google: url - google profile link
     """
-    user = models.ForeignKey(CustomUser, null=True)
-    first_name = models.TextField(null=True)
-    last_name = models.TextField(null=True)
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE,
+        primary_key=True,
+    )
     avatar = models.URLField(null=True)
     age = models.IntegerField(null=True)
     gender = models.TextField(null=True)
@@ -36,14 +37,41 @@ class Profile(models.Model):
         """
         Get user profile with user_id
         Args:
-            user (int): id of user.
+            user_id (int): id of user.
         Returns:
             Object<Profile>: Object of Profile or None if got exception.
         """
         try:
-            return Profile.objects.get(id=user)
+            return Profile.objects.get(user=user_id)
         except ObjectDoesNotExist:
             return None
+
+    def edit(self, data):
+        """
+        Updates Profile with new avatar, age, gender, hobbies, 
+        facebook link, instagram link and google link
+
+         Args:
+            avatar (str): link to avatar.
+            age (int): user age,
+            gender (str): user gender
+            hobbies (str): user hobbies
+            facebook (str): facebook link
+            instagram (str): instagram link
+            google (str): google link
+        Returns:
+            profile obj
+        """
+        self.avatar = data['avatar']
+        self.age = data['age']
+        self.gender = data['gender']
+        self.hobbies = data['hobbies']
+        self.facebook = data['facebook']
+        self.instagram = data['instagram']
+        self.google = data['google']
+        self.save()
+        return True
+
 
     def to_dict(self):
         """Convert model object to dictionary.
@@ -64,8 +92,6 @@ class Profile(models.Model):
         """
         return {
             "user": self.user.id if self.user else None,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
             "avatar": self.avatar,
             "age": self.age,
             "gender": self.gender,
