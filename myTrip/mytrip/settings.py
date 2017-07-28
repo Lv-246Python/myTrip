bd3ee261d8d2ce26fb5fe08d6c59b8b01b9ee3fe
Spycheast <spycheast@gmail.com>
2017-07-28 14:06:40 +0300

    Added profile app to settings and urls

--- myTrip/mytrip/settings.py
+++ myTrip/mytrip/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'registration',
     'photo',
     'home',
+    'profile',
 ]
 
 TEMPLATES = [
--- myTrip/mytrip/urls.py
+++ myTrip/mytrip/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^api/v1/trip/', include('trip.urls')),
     url(r'^api/v1/auth/', include('registration.urls')),
     url(r'^api/v1/photo/', include('photo.urls')),
+    url(r'^api/v1/profile/', include('profile.urls')),
     url(r'', include('home.urls')),
 ]
