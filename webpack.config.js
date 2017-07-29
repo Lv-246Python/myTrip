9a7f3fa56f87013064ff7cec6f52738cd5300f5e
yuriiZherebukh <yura7887@gmail.com>
2017-07-29 19:22:35 +0300

    Configured webpack to resolve less styles, added additional npm packages, changed home component

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var config = {
     entry:   path.join(__dirname, 'myTrip/static/src/app.js'),
@@ -13,9 +14,23 @@ var config = {
             test: /\.js/,
             include: path.join(__dirname, 'myTrip/static/'),
             loader: 'babel-loader'
+        },
+        {
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'less-loader']
+      })
         }
         ]
     },
+    plugins:[
+        new ExtractTextPlugin({
+            filename: 'index.css',
+            disable: false,
+            allChunks: true
+        })
+    ],
 };
 
 module.exports = config;
