5c62fa577252c782115a8e3dd735d51cede686e8
Roman Hrytskiv <roman.hrytskiv.m@gmail.com>
2017-08-01 10:29:34 +0300

    Unique key.prop bug fixed.

--- myTrip/static/src/comment/comment_item.js
+++ myTrip/static/src/comment/comment_item.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
 import FlatButton from 'material-ui/FlatButton';
-import ListItem from 'material-ui/List/ListItem';
 
 const styles = {
   avatar: {
@@ -19,49 +18,37 @@ const styles = {
 };
 
 export class CommentItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      comments: [{a: '2', b: 4}, {a: 1, b: 3}]
+    handleDelete = () => {
+        axios.delete('api/v1/trip/2/comment/9/')
     };
-}
 
-componentDidMount() {
-  axios.get('api/v1/trip/7/comment/')
-    .then(response => {
-      const comments = response.data;
-      this.setState({comments});
+    render() {
+        return (
+          <div>
+              <Card>
+                  <CardHeader
+                      title={this.props.username}
+                      subtitle={this.props.updated}
+                      expandable={true} />
+
+                  <CardText
+                      actAsExpander={true}
+                      style={styles.commentText}>
+                      <Avatar src="static/src/img/avatar.jpg" size={40} style={styles.avatar} />
+                      {this.props.message}
+                  </CardText>
+
+                  <CardActions
+                      expandable={true}>
+                    <FlatButton
+                        onTouchTap={this.handleEdit}
+                        label="Edit" />
+                    <FlatButton
+                        onTouchTap={this.handleDelete}
+                        label="Delete" />
+                  </CardActions>
+              </Card>
+          </div>
+        );
+      }
     }
-    );
-}
-
-render() {
-    return (
-      <div>
-            {this.state.comments.length && this.state.comments.map( comment =>
-                (<ListItem>
-                <Card>
-                    <CardHeader
-                        title={comment.user}
-                        subtitle={comment.update_at}
-                        expandable={true} />
-
-                    <CardText
-                        actAsExpander={true}
-                        style={styles.commentText}>
-                        <Avatar src="static/src/img/avatar.jpg" size={40} style={styles.avatar}/>
-                        {comment.message}
-                    </CardText>
-
-                    <CardActions
-                        expandable={true}>
-                      <FlatButton label="Edit" />
-                      <FlatButton label="Delete" />
-                    </CardActions>
-                </Card>
-            </ListItem>))}
-      </div>
-    );
-  }
-}
