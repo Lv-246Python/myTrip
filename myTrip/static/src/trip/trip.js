1b4777d6d7e4b017ebab95bb53f5022409fd0a15
tayrus <artem.chornyi.ua@gmail.com>
2017-08-02 01:23:18 +0300

    static trip layout

--- myTrip/static/src/trip/trip.js
+++ myTrip/static/src/trip/trip.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import Avatar from 'material-ui/Avatar';
-import Paper from 'material-ui/Paper';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import { Card, CardHeader, CardMedia, CardText, CardActions } from 'material-ui/Card';
+import ChatIcon from 'material-ui-icons/Chat';              // іконка коментів
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';  // іконка стрілочки розгортання
+import IconButton from 'material-ui/IconButton';            // зробити іконки кнопками
+import FavoriteIcon from 'material-ui-icons/Favorite';      // іконка сердечка
+import ShareIcon from 'material-ui-icons/Share';            // іконка share
+
 
 const styles = {
-  avatar: {
-      marginRight: 10,
-      marginBottom: 10
+  card: {
+    maxWidth: 800,
+    margin: 'auto'
   }
 };
 
@@ -16,81 +20,71 @@ class Trip extends React.Component {
    render() {
       return (
          <div>
-            <TripHeader />
-            <TripMap />
+         <Card style={styles.card}>
+
+            <CardHeader
+               title={<h2>Baku journey</h2>}
+               subtitle="Status: announced. Last date update: 5 days ago"
+            />
+
+            <CardMedia>
+                <img src="static/src/img/world_map.jpg"/>
+            </CardMedia>
+
+            <Card style={styles.card}>
             <TripPhotoGallery />
-            <TripMainText />
+            </Card>
+
+            <CardText>
+
+<h4>Description:</h4>
+
+<p />Prior to an Olympics Games, and before the 1st European Games in Baku in 2015, a torch was lit with sacred
+fire and carried around the host country to bring the spirit and excitement of the games to the whole
+country. For the Baku 2017 Islamic Solidarity Games, our objective is the same but this time our Journey
+is not focused on fire but on the element of water.
+
+<p />Water is hugely important in the Islamic faith, symbolising purity, healing and cleansing. Our host city of Baku
+is on the shores of the Caspian Sea. Water is one of the four classical elements that we celebrate before Novruz
+alongside earth, air and fire. All of these points are hugely important and this is why we have chosen to celebrate
+Water in the lead up to the Games.
+
+<p />On 5th April 2017 the Journey from the Caspian Launch Ceremony took place at the Stone Chronicle Museum in Baku.
+Ilham Aliyev, the President of the Republic of Azerbaijan presented water from the Caspian Sea to children from all
+over Azerbaijan. They will then take the water back to their regions as part of the Journey from the Caspian. This
+water represents a memento, from Baku and the Islamic Games, to the regions of Azerbaijan.
+
+<p />The Journey continues and will visit 15 more iconic cities and water locations, covering over 3,000 km. Each region
+will host a Water Ceremony and an evening Water Festival. The evening Water Festivals are free public events and will
+include performances by local performance groups, the international renowned Azerbaijani group Mirvari, with
+spectacular water effects and an exciting fireworks display.
+
+<p />37 days after the Launch Ceremony, the Journey will come to an end at the Baku Olympic Stadium. The water collected
+from all over this amazing country will return to Baku on the 12th May and will welcome the athletes of the Islamic
+Solidarity Games as part of the Opening Ceremony.
+
+<p /><p>#Hashtags</p>
+
+            </CardText>
+
+          <CardActions disableActionSpacing
+           style={{display: 'flex', justifyContent: 'center'}}
+          >
+            <IconButton aria-label="Like it!">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="Share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton aria-label="Comments">
+              <ChatIcon />
+            </IconButton>
+          </CardActions>
+
             <TripComments />
             <TripCheckpoints />
-         </div>
-      );
-   }
-}
 
-
-class TripHeader extends React.Component {
-   render() {
-      return (
-         <Paper>
-            <AuthorName />
-            <AuthorAvatar />
-            <TripTitle />
-            <TripStatus />
-         </Paper>
-      );
-   }
-}
-
-
-class AuthorName extends React.Component {
-   render() {
-      return (
-         <div>
-            <h3>AuthorName</h3>
-         </div>
-      );
-   }
-}
-
-
-class AuthorAvatar extends React.Component {
-   render() {
-      return (
-         <div>
-            <Avatar src="static/src/img/avatar.jpg" size={40} style={styles.avatar}/>
-         </div>
-      );
-   }
-}
-
-
-class TripTitle extends React.Component {
-   render() {
-      return (
-         <div>
-            <h3>TripTitle</h3>
-         </div>
-      );
-   }
-}
-
-
-class TripStatus extends React.Component {
-   render() {
-      return (
-         <div>
-            <h3>TripStatus</h3>
-         </div>
-      );
-   }
-}
-
-
-class TripMap extends React.Component {
-   render() {
-      return (
-         <div>
-            <img src="static/src/img/world_map.jpg" style={{margin:0}} />
+         </Card>
          </div>
       );
    }
@@ -100,43 +94,8 @@ class TripMap extends React.Component {
 class TripPhotoGallery extends React.Component {
    render() {
       return (
-         <Paper>
+         <div>
             <h3>TripPhotoGallery</h3>
-         </Paper>
-      );
-   }
-}
-
-
-class TripMainText extends React.Component {
-   render() {
-      return (
-         <Paper>
-            <TripDescription />
-            <TripHashtags />
-         </Paper>
-      );
-   }
-}
-
-
-
-class TripDescription extends React.Component {
-   render() {
-      return (
-         <div>
-            <h3>TripDescription</h3>
-         </div>
-      );
-   }
-}
-
-
-class TripHashtags extends React.Component {
-   render() {
-      return (
-         <div>
-            <p>#TripHashtags</p>
          </div>
       );
    }
@@ -146,9 +105,20 @@ class TripHashtags extends React.Component {
 class TripComments extends React.Component {
    render() {
       return (
-         <div>
-            <h3>TripComments </h3>
-         </div>
+      <Card>
+        <CardHeader
+          title={<h3>Comments</h3>}
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
       );
    }
 }
@@ -157,9 +127,20 @@ class TripComments extends React.Component {
 class TripCheckpoints extends React.Component {
    render() {
       return (
-         <div>
-            <h3>TripCheckpoints </h3>
-         </div>
+      <Card>
+        <CardHeader
+          title={<h3>Checkpoints</h3>}
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
       );
    }
 }
--- myTrip/static/src/trip/trip_card.js
+++ myTrip/static/src/trip/trip_card.js
@@ -1,116 +1,108 @@
-//// npm install material-design-icons
-//// npm install -S material-ui@next
-//// npm install -S material-ui-icons
-//// npm install typeface-roboto --save
-//// npm install babel-plugin-transform-class-properties babel-plugin-add-module-exports babel-preset-stage-0 --save-dev
-//// npm i --save inline-style-prefixer
-//// npm install simple-assign
-//// npm install -S material-ui
-//
-//import React, { Component } from 'react';
-//import PropTypes from 'prop-types';        // npm install prop-types --save
-//import classnames from 'classnames';        // npm install classnames --save
+// npm install material-ui
+// npm install material-ui-icons
+// npm install typeface-roboto --save
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';        // npm install prop-types --save
+import classnames from 'classnames';        // npm install classnames --save
 //import { withStyles, createStyleSheet } from 'material-ui/styles';        // вбудований CSS
-//import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';    // cards elements
-//import Collapse from 'material-ui/transitions/Collapse';    //
-//import Avatar from 'material-ui/Avatar';                    // аватарка
-//import Typography from 'material-ui/Typography';            // використання різних шрифтів
-//import IconButton from 'material-ui/IconButton';            // зробити іконки кнопками
-//import FavoriteIcon from 'material-ui-icons/Favorite';      // іконка сердечка
-//import ShareIcon from 'material-ui-icons/Share';            // іконка share
-//import ExpandMoreIcon from 'material-ui-icons/ExpandMore';  // іконка стрілочки розгортання
-//
-//
-//const styleSheet = createStyleSheet(theme => ({
-//  card: { maxWidth: 800 },
-//  expand: {
-//    transform: 'rotate(0deg)',
-//    transition: theme.transitions.create('transform', {
-//      duration: theme.transitions.duration.shortest,
-//    }),
-//  },
-//  expandOpen: {
-//    transform: 'rotate(180deg)',
-//  },
-//  avatar: {
-//      marginRight: 10,
-//      marginBottom: 10 },
-//  flexGrow: { flex: '1 1 auto' },
-//}));
-//
-//class TripCard extends Component {
-//  state = { expanded: false };
-//
-//  handleExpandClick = () => {
-//    this.setState({ expanded: !this.state.expanded });
-//  };
-//
-//  render() {
-//    const classes = this.props.classes;
-//
-//    return (
-//      <div>
-//        <Card className={classes.card}>
-//          <CardHeader
-//            avatar={
-//              <Avatar aria-label="Recipe" className={classes.avatar}>
-//                R
-//              </Avatar>
-//            }
-//            title="Trip title"
-//            subheader="last date update"
-//          />
-//          <CardMedia>
-//            <img src="static/src/img/world_map.jpg" alt="World map" />
-//          </CardMedia>
-//          <CardContent>
-//            <Typography component="p">
-//              Description.
-//            </Typography>
-//          </CardContent>
-//          <CardActions disableActionSpacing>
-//            <IconButton aria-label="Add to favorites">
-//              <FavoriteIcon />
-//            </IconButton>
-//            <IconButton aria-label="Share">
-//              <ShareIcon />
-//            </IconButton>
-//            <div className={classes.flexGrow} />
-//            <IconButton
-//              className={classnames(classes.expand, {
-//                [classes.expandOpen]: this.state.expanded,
-//              })}
-//              onClick={this.handleExpandClick}
-//              aria-expanded={this.state.expanded}
-//              aria-label="Show more"
-//            >
-//              <ExpandMoreIcon />
-//            </IconButton>
-//          </CardActions>
-//          <Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>
-//            <CardContent>
-//              <Typography paragraph type="body2">
-//                Method:
-//              </Typography>
-//              <Typography paragraph>
-//                On this trip you’ll get to know a diversified and fascinating French region where the weather
-//                changes as quickly as the landscape. As one of France’s most rugged coastlines, Brittany is
-//                renowned the world over for its spectacular rock formations and secluded bays, alternating with
-//                beautiful sandy beaches, medieval walled towns, castles and historic cathedrals, alongside unique
-//                stone houses in quiet, romantic villages. Due to a well-developed infrastructure with countless
-//                campgrounds and shopping facilities, short distances between the sights, and so much to discover
-//                and explore, Brittany is an exiting, picture-perfect family destination.
-//              </Typography>
-//            </CardContent>
-//          </Collapse>
-//        </Card>
-//      </div>
-//    );
-//  }
-//}
-//
-//TripCard.propTypes = {
-//  classes: PropTypes.object.isRequired,
-//};
-//
-//export default withStyles(styleSheet)(TripCard);
+import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';    // cards elements
+import Avatar from 'material-ui/Avatar';                    // аватарка
+import IconButton from 'material-ui/IconButton';            // зробити іконки кнопками
+import FavoriteIcon from 'material-ui-icons/Favorite';      // іконка сердечка
+import ShareIcon from 'material-ui-icons/Share';            // іконка share
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';  // іконка стрілочки розгортання
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+const styleSheet = createStyleSheet(theme => ({
+  card: { maxWidth: 800 },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+      marginRight: 10,
+      marginBottom: 10 },
+  flexGrow: { flex: '1 1 auto' },
+}));
+
+class TripCard extends Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+  render() {
+    const classes = this.props.classes;
+
+    return (
+      <div>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="Recipe" className={classes.avatar}>
+                R
+              </Avatar>
+            }
+            title="Trip title"
+            subheader="last date update"
+          />
+          <CardMedia>
+            <img src="static/src/img/world_map.jpg" alt="World map" />
+          </CardMedia>
+          <CardContent>
+            <div>
+              Description.
+            </div>
+          </CardContent>
+          <CardActions disableActionSpacing>
+            <IconButton aria-label="Add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="Share">
+              <ShareIcon />
+            </IconButton>
+            <div className={classes.flexGrow} />
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded,
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <CardText expandable={true}>
+            <CardContent>
+              <div>
+                <p>
+                On this trip you’ll get to know a diversified and fascinating French region where the weather
+                changes as quickly as the landscape. As one of France’s most rugged coastlines, Brittany is
+                renowned the world over for its spectacular rock formations and secluded bays, alternating with
+                beautiful sandy beaches, medieval walled towns, castles and historic cathedrals, alongside unique
+                stone houses in quiet, romantic villages. Due to a well-developed infrastructure with countless
+                campgrounds and shopping facilities, short distances between the sights, and so much to discover
+                and explore, Brittany is an exiting, picture-perfect family destination.
+                </p>
+              </div>
+            </CardContent>
+          </CardText>
+        </Card>
+      </div>
+    );
+  }
+}
+
+TripCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default TripCard;
