4fed7682304f600bba8d49bbd4998a78ecfa079a
yuriiZherebukh <yura7887@gmail.com>
2017-08-10 13:08:43 +0300

    Fixed help component

--- myTrip/static/src/home/help/message_buttons.js
+++ myTrip/static/src/home/help/message_buttons.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import Snackbar from 'material-ui/Snackbar';
 
 import { postHelp } from './help.service';
 import { emailIsNotValid, fieldIsEmpty } from './../../utils';
@@ -13,13 +14,12 @@ const style = {
     }
 };
 
-const CLEAR_RESPONSE_TEXT_TIME = 3000;
-
 export default class MessageButtons extends React.Component{
 
     constructor(props){
         super(props);
         this.state={
+            open: false,
             to:'',
             subject:'',
             message:'',
@@ -34,6 +34,13 @@ export default class MessageButtons extends React.Component{
         this.setState({[event.target.name]: Value});
     };
 
+    //method for Snackbar to resolve closing
+    handleRequestClose = () => {
+        this.setState({
+          open: false
+        });
+    };
+
     helpPostEvent = (event) => {
         const to = this.state.to;
         const subject = this.state.subject;
@@ -44,7 +51,10 @@ export default class MessageButtons extends React.Component{
         if ( !emailValidation && !subjectValidation && !messageValidation ) {
             postHelp(to, subject, message)
                 .then((response) => {
-                    this.setState({'responseText': response.data});
+                    this.setState({
+                        'responseText': response.data,
+                        'open': true
+                    });
                 })
         } else {
             this.setState({'emailError': emailValidation});
@@ -61,36 +71,43 @@ export default class MessageButtons extends React.Component{
                <span className="helpText">Send your feedback</span>
            </Paper>
            <TextField
-            hintText="Email"
-            floatingLabelText="Enter your email"
-            className="helpTextInput"
-            name="to"
-            onChange={this.onChange}
-            errorText={this.state.emailError}
+                hintText="Email"
+                floatingLabelText="Enter your email"
+                className="helpTextInput"
+                name="to"
+                onChange={this.onChange}
+                errorText={this.state.emailError}
            /><br/>
            <TextField
-            hintText="Subject"
-            floatingLabelText="Enter your subject"
-            className="helpTextInput"
-            name="subject"
-            onChange={this.onChange}
-            errorText={this.state.subjectError}
+                hintText="Subject"
+                floatingLabelText="Enter your subject"
+                className="helpTextInput"
+                name="subject"
+                onChange={this.onChange}
+                errorText={this.state.subjectError}
            /><br/>
             <TextField
-            hintText="Message Field"
-            floatingLabelText="Enter your message"
-            multiLine={true}
-            rows={1}
-            className="helpTextInput"
-            name="message"
-            onChange={this.onChange}
-            errorText={this.state.messageError}
+                hintText="Message Field"
+                floatingLabelText="Enter your message"
+                multiLine={true}
+                rows={1}
+                className="helpTextInput"
+                name="message"
+                onChange={this.onChange}
+                errorText={this.state.messageError}
             /><br/>
-           <RaisedButton label="Send" primary={true}
-                         onTouchTap={this.helpPostEvent}
-                         style={style.button}
+           <RaisedButton
+               label="Send" primary={true}
+               onTouchTap={this.helpPostEvent}
+               style={style.button}
            />
             <span className="helpResponseText">{this.state.responseText}</span>
+            <Snackbar
+              open={this.state.open}
+              message="Event added to your calendar"
+              autoHideDuration={1000}
+              onRequestClose={this.handleRequestClose}
+            />
             </div>
         )
     }
