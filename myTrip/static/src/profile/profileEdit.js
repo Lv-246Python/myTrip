e494d362ac544ad6e575d85ed3ac27aa4cf53661
Ashiato <regens4@gmail.om>
2017-09-21 17:21:48 +0300

    fixed datepicker maxdate

--- myTrip/static/src/profile/profileEdit.js
+++ myTrip/static/src/profile/profileEdit.js
@@ -121,6 +121,7 @@ export class ProfileEdit extends React.Component {
           ref='dp'
           onChange={this.handleChangeBirthday}
           openToYearSelection={true}
+          maxDate={new Date()}
         />
         </div>
 
